bacc3a5001fe9a40e2ea0bf54e6787c79d021728
Ruslan Harvasyuk <harvasyuk@gmail.com>
2019-07-30 00:20:20 +0300

    fix

--- last_ph.js
+++ last_ph.js
@@ -658,12 +658,13 @@ var UiManager = (function () {
 
             game.headerGroup.add(this.install_button)
 
-            this.install_button_label = game.add.text(0, 0, "PLAY_NOW".toUpperCase())
+            //this.install_button_label = game.add.text(0, 0, config.LANG["INSTALL"].toUpperCase())
+            this.install_button_label = game.add.text(0, 0, "PLAY NOW")
             this.install_button_label.fill = "#ffffff"
             this.install_button_label.fontSize = this.install_button.height 
-            this.install_button_label.anchor.setTo(0.5, 0.45)
-            this.install_button.addChild(this.install_button_label)
+            this.install_button.scale.set(0.4)
 
+            this.install_button.addChild(this.install_button_label)
             utils.centerGameObjects([this.install_button, this.install_button_label])
 
             this.install_button.inputEnabled = true
